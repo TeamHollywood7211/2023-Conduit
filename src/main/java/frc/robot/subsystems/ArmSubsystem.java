050b58249f15368c2e8ac2610b5aaff88dddcf40
Eggy-T <eighdin@myisdept.com>
2023-02-24 12:18:12 -0500

    adjust arm speeds
    need to reset the arm position constants

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -138,11 +138,11 @@ public class ArmSubsystem extends SubsystemBase {
   }
 
   public void runGripOut(){
-    gripMotor.set(0.35);
+    gripMotor.set(0.5);
   }
 
   public void runGripIn(){
-    gripMotor.set(-0.35);
+    gripMotor.set(-0.5);
   }
 
   public void runGripInPrecise(){
