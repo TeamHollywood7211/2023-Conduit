4bb34af1ab585c8022e6649c5d57d3e614cf5a1d
Software Poet <baronz@usa.net>
2023-07-12 20:06:02 -0400

    Practice Bot Offsets
    Offsets For Practice Bot

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,21 +14,21 @@ public final class Constants {
     public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1;
     public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 2;
     public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 3;
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(85.25); // 85.25 for practice bot 177.626 for new
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(85.25+90); // 85.25 for practice bot 177.626 for new
 
     public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 11; 
     public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 12; 
     public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 13;
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(25.92); // 25.92 for practice bot 11.953 for new
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(10.458); // 25.92 for practice bot 11.953 for new
     public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 21;  
     public static final int BACK_LEFT_MODULE_STEER_MOTOR = 22;  
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 23;
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(57.21); // 57.21 for practice bot 252.333 for new
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(254); // 57.21 for practice bot 252.333 for new
                                                                 
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 31;  
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 32;  
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 33;
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(159.61); // 159.61 for practice bot -87.515 for new
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(275.097); // 159.61 for practice bot -87.515 for new
 
     public static final int DRIVE_MOTOR_CURRENT_LIMIT = 40;
     public static final int STEER_MOTOR_CURRENT_LIMIT = 20;
